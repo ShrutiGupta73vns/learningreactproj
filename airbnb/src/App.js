34d09dfb645cf499aa5d695173a75df10b0e9b2f
Shruti Gupta <shrurose73@gmail.com>
2023-10-01 15:03:48 +0530

    dynamism added

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -1,43 +1,39 @@
 
 import './App.css';
-import katieatie from './assets/Katie.png' 
+
 import  Wedding from './assets/Wedding.jpg' 
 import katie from './assets/Katie.png' 
 import Navbar from './components/Navbar';
 import Body from './components/Body';
 import Card from './components/Card';
-import  Bike from './assets/Bike.jpg' 
+import  Bike from './assets/Bike.jpg' ;
+import data from './assets/data'
 
 function App() {
+  const cards =data.map(card => {
+    return(
+      <Card
+      key={card.id}
+      img={card.img}
+      rating ={card.rating}
+      reviewcount={card.reviewcount}
+      country={card.country}
+      title={card.title}
+      price={card.price} 
+      />
+       
+    )
+  }
+    )
+
   return (
     <div className="App">
       <Navbar/>
       <Body/>
       <div className='card_container'>
-      <Card
-      img={katie}
-      rating ={5.0}
-      reviewcount={6}
-      country="USA"
-      title="Life lessons with Katie Zaferes"
-      price={136} 
-      />
-     <Card
-      img={Wedding}
-      rating ={5.0}
-      reviewcount={30}
-      country="USA"
-      title="Learn wedding photography"
-      price={125} 
-      />
-      <Card
-      img={Bike}
-      rating ={4.8}
-      reviewcount={2}
-      country="USA"
-      title="Group Mountain biking"
-      price={50} 
-      />
+      
+     
+      {cards}
         </div>
     </div>
   );
---
+++ airbnb/src/assets/data.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    id: 1,
+    img: "Katie.jpg",
+    rating: 5.0,
+    reviewcount: 6,
+    country: "USA",
+    title: "Life lessons with Katie Zaferes",
+    price: 136,
+  },
+  {
+    id: 2,
+    img: "Wedding.jpg",
+    rating: 5.0,
+    reviewcount: 30,
+    country: "USA",
+    title: "Learn wedding photography",
+    price: 125,
+  },
+  {
+    id: 3,
+    img: "bike.jpg",
+    rating: 4.8,
+    reviewcount: 2,
+    country: "USA",
+    title: "Group Mountain biking",
+    price: 50,
+  },
+];
