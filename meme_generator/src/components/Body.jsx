06d271c894c59e5ff23f9a2a4c4e7e36b0586659
Shruti Gupta <shrurose73@gmail.com>
2023-10-10 17:58:49 +0530

    button added anded styled

--- meme_generator/src/components/Body.jsx
+++ meme_generator/src/components/Body.jsx
@@ -4,9 +4,11 @@ import TextField from '@mui/material/TextField';
 
 export default function Body(){
     return(
+        <div className='componenets'>
         <div className='inputbox'>
-             <TextField className='box1' id="outlined-search" label="" type="search" />
-             <TextField  className='box2' id="outlined-search" label="" type="search" />
+             <TextField className='box1' id="outlined-search" label="top text" type="search" />
+             <TextField  className='box2' id="outlined-search" label="bottom text" type="search" /></div>
+            <div className='buttondiv' > <button  className='button'><p className='buttontext'> Get a new meme image 🖼</p></button></div>
         </div>
     )
 
